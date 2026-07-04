6b9d203d835aece668aec4c9306fc4786d993b6f
André Guilherme <andreguilherme645@gmail.com>
2026-07-04 16:45:57 -0300

    initial commit

---
+++ src/main/java/com/andre/crud_spring_back/CrudSpringBackApplication.java
@@ -0,0 +1,13 @@
+package com.andre.crud_spring_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andre/crud_spring_back/CrudSpringBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.crud_spring_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
