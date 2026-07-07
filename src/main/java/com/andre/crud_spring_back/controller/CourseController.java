35c456937a6fe1cf53830645b501e05b5ec428b5
André Guilherme <andreguilherme645@gmail.com>
2026-07-06 21:17:20 -0300

    Criando Controller de Cursos

---
+++ src/main/java/com/andre/crud_spring_back/controller/CourseController.java
@@ -0,0 +1,26 @@
+package com.andre.crud_spring_back.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andre.crud_spring_back.model.Course;
+import com.andre.crud_spring_back.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+
+public class CourseController {
+    private final CourseRepository courseRepository;
+    
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
