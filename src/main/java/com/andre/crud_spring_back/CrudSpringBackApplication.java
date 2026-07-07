c7752accd175d021f99277377b363673b9e8ce3e
André Guilherme <andreguilherme645@gmail.com>
2026-07-06 21:17:35 -0300

    Criando resposta para o endpoint teste

--- src/main/java/com/andre/crud_spring_back/CrudSpringBackApplication.java
+++ src/main/java/com/andre/crud_spring_back/CrudSpringBackApplication.java
@@ -1,7 +1,12 @@
 package com.andre.crud_spring_back;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.andre.crud_spring_back.model.Course;
+import com.andre.crud_spring_back.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringBackApplication {
@@ -10,4 +15,16 @@ public class CrudSpringBackApplication {
 		SpringApplication.run(CrudSpringBackApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("FrontEnd");
+
+			courseRepository.save(c);
+		};
+	}
 }
