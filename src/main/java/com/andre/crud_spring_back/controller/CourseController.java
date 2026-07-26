fc2f5492e1a69d4f003651963b50e3c1a3e95a02
André Guilherme <andreguilherme645@gmail.com>
2026-07-26 15:53:45 -0300

    Criando endpoint de POST Courses

--- src/main/java/com/andre/crud_spring_back/controller/CourseController.java
+++ src/main/java/com/andre/crud_spring_back/controller/CourseController.java
@@ -1,6 +1,10 @@
 package com.andre.crud_spring_back.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,10 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 }
