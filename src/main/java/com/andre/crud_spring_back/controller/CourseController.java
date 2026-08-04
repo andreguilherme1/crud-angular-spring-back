2c9fcfa800699f04253ce94ab473096009f0e3d0
André Guilherme <andreguilherme645@gmail.com>
2026-08-03 22:58:02 -0300

    Criando endpoint para deletar um curso

--- src/main/java/com/andre/crud_spring_back/controller/CourseController.java
+++ src/main/java/com/andre/crud_spring_back/controller/CourseController.java
@@ -2,6 +2,7 @@ package com.andre.crud_spring_back.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,6 +53,17 @@ public class CourseController {
                 Course updated = courseRepository.save(courseFound);
                 
                 return ResponseEntity.ok().body(updated);
+            })
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id) {
+        return courseRepository.findById(id)
+            .map(courseFound -> {
+                courseRepository.delete(courseFound);
+
+                return ResponseEntity.noContent().<Void>build();
             })
             .orElse(ResponseEntity.notFound().build());
     }
